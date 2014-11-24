1c5c904327b5376e569a80bfc641b936f7f0ecb7
fiendfan1 <fiendfan1@yahoo.com>
2014-11-24 09:43:24 -0800

    Removed old files

--- Language/GLSL/Monad.hs
+++
@@ -1,6 +0,0 @@
-module Language.GLSL.Monad (
-    module Language.GLSL.Monad.GLSL,
-    module Language.GLSL.Monad.Primitive
-) where
-import Language.GLSL.Monad.GLSL
-import Language.GLSL.Monad.Primitive
